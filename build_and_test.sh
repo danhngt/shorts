28f82e5c963147c624f80a4e93e861e0c0222aa2
danhngt <thanhdanh.nguyen@manabie.com>
2022-03-10 17:14:30 +0700

    .

---
+++ build_and_test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+previous_branch=$(git rev-parse --abbrev-ref HEAD)
+test_file="/tmp/test.sh"
+
+test_folders() {
+	xargs dirname \
+	| grep -P "^features/" \
+	| grep -v "common" \
+	| sort -u
+}
+test_services() {
+	test_folders \
+	| sed 's/^[^/]*\///' \
+	| sed 's/\/.*$//' \
+	| sort -u
+}
+
+base_branch=$(\
+	git show-branch \
+	| sed "s/].*//" \
+	| grep "\*" \
+	| grep -v "$previous_branch" \
+       	| head -n1 \
+       	| sed "s/^.*\[//" \
+)
+folders=$(
+	git diff "$base_branch" "$previous_branch" --name-only \
+	| test_folders \
+	| sort -u \
+)
+services="$(\
+	git diff "$base_branch" "$previous_branch" --name-only \
+	| test_services \
+	| sort -u \
+	| xargs -i echo -e "\t{} \\"
+)"
+
+echo -e "
+#!/bin/bash
+./deployments/build.bash
+./deployments/build.bash \\
+${services}
+;
+" > "$test_file"
+
+for folder in $folders; do
+    echo "manabie.run ./deployments/k8s_bdd_test.bash $folder" >> "$test_file"
+done
+
+echo '
+notify-send -i emblem-default "Done"
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+' >> "$test_file"
+
+vi "$test_file"
+bash "$test_file"
